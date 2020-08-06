103550b12996ee59f141bab961302a7b72075458
Vlad <67093050+Ascilator@users.noreply.github.com>
2020-08-06 13:29:49 +0300

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,47 @@
+﻿jQuery(function () {
+
+
+	$('.next').click(function(){
+		$('.mounth_number').removeClass('_active');
+		var year_item = $('.year_item:first-child').clone();
+		var title = $('.year_item:first-child').children('.number_of_year').text();
+		var title_2 = +title;
+		$('.middle_part').append(year_item);
+		$('.middle_part>.year_item:last-child').children('.number_of_year').text(title_2+2);
+		$('.middle_part>.year_item:first-child').remove();
+
+		
+		
+	});
+	$('.prev').click(function(){
+		$('.mounth_number').removeClass('_active');
+		var year_item = $('.year_item:last-child').clone();
+		var title = $('.year_item:last-child').children('.number_of_year').text();
+		var title_2 = +title;
+		$('.middle_part').prepend(year_item);
+		$('.middle_part>.year_item:first-child').children('.number_of_year').text(title_2-2);
+		$('.middle_part>.year_item:last-child').remove();
+	});
+
+
+
+	$('.cancel_btn').click(function(){
+		$('.mounth_number').removeClass('_active');
+		$('.result_part').text('');
+	});
+	$('body').on('click',' .mounth_number', function(){
+
+		var current_year = $(this).parent().parent().siblings('.number_of_year').text(),
+			current_mounth = $(this).children().text();
+
+		$('.result_part').text(current_mounth + ' ' + current_year);
+		$('.mounth_number').removeClass('_active');
+
+
+
+
+		$(this).addClass('_active');
+	});
+});
+
+
